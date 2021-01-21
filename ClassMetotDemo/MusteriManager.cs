bcf1ebb1d8d5018c2cb283c4ef66daf34aeeff5b
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-01-22 00:20:31 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Sayın " + musteri.Ad + " " + musteri.Soyad + ";\nKaydınız Eklendi.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Sayın " + musteri.Ad + " " + musteri.Soyad + 
+                ";\nBilgileriniz Şu Şekildedir : " + 
+                "\nAdınız Soyadınız : " + musteri.Ad + " " + musteri.Soyad + 
+                "\nAdresiniz : " + musteri.Adres + 
+                "\nCinsiyetiniz : " + musteri.Cinsiyet + 
+                "\nBoyunuz : " + musteri.Boy);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Sayın " + musteri.Ad + " " + musteri.Soyad + ";\nSilme İşleminiz Başarılı");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,36 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 100;
+            musteri1.Ad = "Mehmet Akif";
+            musteri1.Soyad = "TURAN";
+            musteri1.Adres = "Balıkesir";
+            musteri1.Cinsiyet = true;
+            musteri1.Boy = 1.75;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 101;
+            musteri2.Ad = "Irmak Nisa";
+            musteri2.Soyad = "YILMAZ";
+            musteri2.Adres = "Ordu";
+            musteri2.Cinsiyet = false;
+            musteri2.Boy = 1.55;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 102;
+            musteri3.Ad = "Ali Kağan";
+            musteri3.Soyad = "FATİH";
+            musteri3.Adres = "Ankara";
+            musteri3.Cinsiyet = true;
+            musteri3.Boy = 1.85;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            Console.WriteLine("----------------------------------");
+            musteriManager.Listele(musteri2);
+            Console.WriteLine("----------------------------------");
+            musteriManager.Sil(musteri3);
         }
     }
 }
